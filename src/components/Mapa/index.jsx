e635ea4db382dbcfec63f9c87ab70013e3094e17
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-08-30 20:00:45 -0300

    new feature

--- src/components/Mapa/index.jsx
+++ src/components/Mapa/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 function Mapa({localLink}){
     return (
         <div className='mapa'>
-            <iframe src={localLink} width="90%" height="200" loading="lazy"/>
+            <iframe src={localLink} width="99%" height="200" loading="lazy"/>
         </div>
     )
 }
--- src/pages/InvitePage/index.jsx
+++ src/pages/InvitePage/index.jsx
@@ -38,17 +38,20 @@ function InvitePage() {
             <span></span>
 
             <div className='infos-container'>
-              <div className='cerimonia'>
-                <img onClick={()=> setMapOpen(!mapOpen)} src={cerimoniaImg} />
-                {mapOpen && <Mapa localLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3676.935704698311!2d-43.337612225434754!3d-22.841868135552723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9963519a8bb29f%3A0xe9da8c2a33b76399!2sEstr.%20do%20Col%C3%A9gio%2C%2065%20-%20Col%C3%A9gio%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021235-280!5e0!3m2!1spt-BR!2sbr!4v1693414644847!5m2!1spt-BR!2sbr"} />}
+              <div className='icons'>
+                <div className='cerimonia'>
+                  <img onClick={()=> setMapOpen(!mapOpen)} src={cerimoniaImg} />
+                </div>
+                <div>
+                  <img src={recepcaoImg} />
+                  <span></span>
+                </div>
+                <div>
+                  <img src={confirmacaoImg} />  
+                </div>
               </div>
-              <div>
-                <img src={recepcaoImg} />
-                <span></span>
-              </div>
-              <div>
-                <img src={confirmacaoImg} />  
-              </div>
+              {mapOpen && <Mapa localLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3676.935704698311!2d-43.337612225434754!3d-22.841868135552723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9963519a8bb29f%3A0xe9da8c2a33b76399!2sEstr.%20do%20Col%C3%A9gio%2C%2065%20-%20Col%C3%A9gio%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021235-280!5e0!3m2!1spt-BR!2sbr!4v1693414644847!5m2!1spt-BR!2sbr"} />}
+              
             </div>
         </section>
         }
