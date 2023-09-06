9eebf659cf371dd5b427fa60ae3464f650d9d5ab
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-09-06 11:23:17 -0300

    adiciona fonte eb garamond e link para whatsapp

--- src/components/Mapa/index.jsx
+++ src/components/Mapa/index.jsx
@@ -3,6 +3,7 @@ import './style.css'
 function Mapa({localLink}){
     return (
         <div className='mapa'>
+            <div className='seta'></div>
             <iframe src={localLink} width="100%" height="100%" loading="lazy"/>
         </div>
     )
--- src/pages/InvitePage/index.jsx
+++ src/pages/InvitePage/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import './style.css'
 import music from '../../assets/music.mp3'
 import ModalAudio from '../../components/ModalAudio'
@@ -6,16 +6,18 @@ import flores from '../../assets/flores.png'
 import pvENoivaImg from '../../assets/pvenoiva.png'
 import musicaIcon from '../../assets/musicaIcon.png'
 import cerimoniaImg from '../../assets/igreja.png'
-import recepcaoImg from '../../assets/recepcao.png'
 import confirmacaoImg from '../../assets/confirmacao.png'
 import Mapa from '../../components/Mapa'
-import flores2 from '../../assets/flores2.png'
-import MensagensModal from '../../components/MensagensModal'
+import { useNavigate } from 'react-router-dom'
+
+
 function InvitePage() {
+  const navigate = useNavigate()
   const musicRef = useRef(null)
   const [showModal, setShowModal] = useState(true)
   const [mapOpen, setMapOpen] = useState(false)
   const [confirmOpen, setConfirmOpen] = useState(false)
+
   function StopOrPlayMusic(){
     if(!musicRef.current.src){
       musicRef.current.src = music
@@ -26,6 +28,14 @@ function InvitePage() {
     }
     musicRef.current.pause()
   }
+
+  function enviarMsgWhatsapp(){
+    const text = "https://api.whatsapp.com/send?phone=5521966402513&text=Ol%C3%A1,%20sou%20(Seu%20nome)%20e%20quero%20confirmar%20minha%20presen%C3%A7a%20no%20casamento%20da%20Lydia%20e%20Paulo%20Victor."
+    const newTab = window.open(text, '_blank');
+    newTab.focus();
+  }
+
+
   return (
     <>
     <div className="container">
@@ -37,21 +47,17 @@ function InvitePage() {
               <h3 className='versiculo'>Grandes coisas fez o Senhor por nós, e, por isso, estamos alegres.<br/>(Salmos 126:3)</h3>
               <img className='img-noivos' src={pvENoivaImg} />
               <h3 className='invite-h3'>CONVIDAM PARA CELEBRAR O SEU CASAMENTO</h3>
-              <span></span>
-              <span></span>
             </div>
 
             <div className='infos-container'>
               <div className='icons'>
                 <div className='cerimonia'>
                   <img onClick={()=> setMapOpen(!mapOpen)} src={cerimoniaImg} />
+                  <span>Celebração</span>
                 </div>
-                <div>
-                  <img src={recepcaoImg} />
-                  <span></span>
-                </div>
-                <div>
-                  <img onClick={()=> setConfirmOpen(!confirmOpen)} src={confirmacaoImg} />  
+                <div className='confirmacao'>
+                  <img onClick={()=>  enviarMsgWhatsapp()} src={confirmacaoImg} /> 
+                  <span>Confirmação</span> 
                 </div>
               </div>
               {mapOpen && <Mapa localLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3676.935704698311!2d-43.337612225434754!3d-22.841868135552723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9963519a8bb29f%3A0xe9da8c2a33b76399!2sEstr.%20do%20Col%C3%A9gio%2C%2065%20-%20Col%C3%A9gio%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021235-280!5e0!3m2!1spt-BR!2sbr!4v1693414644847!5m2!1spt-BR!2sbr"} />}
